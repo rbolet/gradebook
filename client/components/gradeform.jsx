657381c216dcd5138dc590f526581e6d9deb1fb0
re-bo <dev@404-NOTFOUND.localdomain>
2019-11-15 16:56:23 -0800

    Weird POST thing fixed

--- client/components/gradeform.jsx
+++ client/components/gradeform.jsx
@@ -21,7 +21,8 @@ class GradeForm extends React.Component {
     this.setState({ [name]: newValue });
   }
 
-  onAdd() {
+  onAdd(event) {
+    event.preventDefault();
     const newGradeEntry = Object.assign({}, this.state);
     this.props.onSubmit(newGradeEntry);
     this.onClear();
@@ -37,7 +38,7 @@ class GradeForm extends React.Component {
   render() {
     return (
       <div className="grade-form container col-md-3 d-flex flex-column align-items-end">
-        <form className="add-grade-form ">
+        <form onSubmit={this.onAdd} className="add-grade-form ">
           <div className="d-flex flex-row align-items-center justify-content-end  mb-2">
             <i className="fas fa-user col-1"></i><input onChange={this.onInputChange} className="mx-2" type="text" name="newName" placeholder="Name" required/>
           </div>
@@ -48,7 +49,7 @@ class GradeForm extends React.Component {
             <i className="fas fa-graduation-cap"></i><input onChange={this.onInputChange} className="mx-2" type="text" name="newGrade" placeholder="Grade" required/>
           </div>
           <div className="btn-group-md d-flex flex-row justify-content-end" role="group" >
-            <button onClick={this.onAdd} className="btn btn-outline-secondary mx-1" type="submit">Add</button>
+            <button className="btn btn-outline-secondary mx-1" type="submit">Add</button>
             <button onClick={this.onClear}className="btn btn-outline-secondary mx-1" type="reset">Clear</button>
           </div>
         </form>
